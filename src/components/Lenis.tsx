57ad35aaffec34e9db41cddd44cf31529882bd25
Khawar Sultan <khawarsultan.developer@gmail.com>
2024-08-25 15:16:32 +0500

    changes lenis

--- src/components/Lenis.tsx
+++ src/components/Lenis.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useEffect } from 'react';
-import Lenis from '@studio-freight/lenis';
+import Lenis from 'lenis';
 export default function SmoothScroller() {
     useEffect(() => {
         const lenis = new Lenis()

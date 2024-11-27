dd5bfc88b71db69df7fb77dc6c21ad6fca841ca7
Khawar Sultan <khawarsultan663@gmail.com>
2024-11-27 17:23:41 +0500

    resume fixes

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -64,7 +64,7 @@ export const DATA = {
       },
       resume: {
         name: "Resume",
-        url: "https://drive.google.com/file/d/1gbyfOxEgwfUzyhR2HG2YojMjsboXtXwa/view?usp=sharing",
+        url: "https://drive.google.com/file/d/1PW3ckJ-La4pYxVM9KLBhEADS4CFLK-xg/view?usp=sharing",
         icon: Icons.resume,
         navbar: true,
       },
